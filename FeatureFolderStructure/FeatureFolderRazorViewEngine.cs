2f27ed76f8fb1759ba894c8586ffcc83aa151bfc
Anshu <anshu.a.bhatnagar@capgemini.com>
2018-07-31 19:40:55 +0530

    Added code for Feature wise Folder Structure.

---
+++ FeatureFolderStructure/FeatureFolderRazorViewEngine.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+
+namespace FeatureFolderStructure
+{
+    public class FeatureFolderRazorViewEngine : RazorViewEngine
+    {
+        public FeatureFolderRazorViewEngine() : base()
+        {
+            /* Parameters:
+             * {2} - Area Name
+             * {1} - Controller Name
+             * {0} - View Name
+             */
+
+            var featureFolderViewLocationFormats = new string[]
+            {
+                "~/Features/{1}/{0}.cshtml",
+                "~/Features/Shared/{0}.cshtml"
+            };
+
+            ViewLocationFormats = featureFolderViewLocationFormats;
+            MasterLocationFormats = featureFolderViewLocationFormats;
+            PartialViewLocationFormats = featureFolderViewLocationFormats;
+
+            var areaFeatureFolderViewLocationFormats = new string[]
+            {
+                "~/Areas/{2}/{1}/{0}.cshtml",
+                "~/Areas/{2}/Shared/{0}.cshtml"
+            };
+
+            AreaViewLocationFormats = areaFeatureFolderViewLocationFormats;
+            AreaMasterLocationFormats = areaFeatureFolderViewLocationFormats;
+            AreaPartialViewLocationFormats = areaFeatureFolderViewLocationFormats;
+        }
+    }
+}
---
+++ FeatureFolderStructure/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace FeatureFolderStructure
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new FeatureFolderRazorViewEngine());
+        }
+    }
+}
